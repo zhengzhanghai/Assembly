b68f48f5b89a3fd362f4bdacd1250ddc28782503
zhengzhanghai <zzh_xiaohai@163.com>
2020-08-06 18:24:53 +0800

    调整文件结构

---
+++ ARMAssemblyDemo/ARMAssemblyDemo/MainAndAssembly/main.m
@@ -0,0 +1,30 @@
+//
+//  main.m
+//  ARMAssemblyDemo
+//
+//  Created by 郑章海 on 2020/8/6.
+//  Copyright © 2020 zzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+
+void Des();
+
+int main(int argc, char * argv[]) {
+    
+    Des();
+    
+    return 0;
+}
+
+
+
+//int main(int argc, char * argv[]) {
+//    NSString * appDelegateClassName;
+//    @autoreleasepool {
+//        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+//    }
+//    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+//}
--- ARMAssemblyDemo/ARMAssemblyDemo/MainAndAssembly/test2.s
+++ ARMAssemblyDemo/ARMAssemblyDemo/MainAndAssembly/test2.s
@@ -1,7 +1,10 @@
 
-//.text
+.text
+.global _Des;
 
-//.global _Des;
-
-//_Des:
-//    sub
+_Des:
+    sub sp, sp , #0x20
+    stp x0, x1, [sp, #0x10]
+    ldp x1, x0, [sp, #0x10]
+    add sp, sp, #0x20
+    ret
